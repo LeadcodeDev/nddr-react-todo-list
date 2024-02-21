1e3431fcc98f1c61cd5a939707c83ede24d8db87
Baptiste Parmantier <baptiste.parmantier.pro@gmail.com>
2024-02-21 19:46:35 +0100

    chore(ui): add icons

--- unocss.config.ts
+++ unocss.config.ts
@@ -1,4 +1,9 @@
-import { defineConfig } from 'unocss'
+import { defineConfig, presetIcons, presetUno, presetAttributify } from 'unocss'
 
 export default defineConfig({
+  presets: [
+    presetUno(),
+    presetAttributify(),
+    presetIcons()
+  ],
 })
